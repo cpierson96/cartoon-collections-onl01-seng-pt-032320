3ab0bf25ab2cf8d11c27fdaeab5e5ae9e460b07f
Claudia Pierson <c172@umbc.edu>
2020-04-13 03:27:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,15 +2,15 @@ def roll_call_dwarves(name)
   name.each_with_index{|word,index| puts "#{index+1}, #{word}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect{|word|word.capitalize+"!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any?{|word|word.length>4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find{|cheese|cheese_types.include?(cheese)}
 end
